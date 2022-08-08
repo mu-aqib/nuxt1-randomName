616e4cfe3d7a4675abb19402ef050ba9c83af99d
Muhammad Aqib <aqibafridi0345@gmail.com>
2022-08-08 17:11:38 +0500

    component restyling with new design

--- app.vue
+++ app.vue
@@ -1,63 +1,58 @@
 <script setup lang="ts">
-  import {Gender, Popularity, Length, names} from '@/data'
+  import {
+    Gender,
+    Popularity,
+    Length,
+    names
+  } from '@/data'
 
   interface OptionsState {
     gender: Gender,
-    popularity: Popularity,
-    length: Length
+      popularity: Popularity,
+      length: Length
   }
 
-  const options = reactive<OptionsState>({
+  const options = reactive < OptionsState > ({
     gender: Gender.BOY,
     popularity: Popularity.UNIQUE,
     length: Length.LONG
   })
-  
-  const generatedName = ref<String[]>([])
 
-  const filterName = ()=>{
-    const element = names.filter( name=> options.gender === name.gender)
+  const generatedName = ref < String[] > ([])
+
+  const filterName = () => {
+    const element = names.filter(name => options.gender === name.gender)
       .filter(name => name.popularity === options.popularity)
-      .filter(name =>{
-        if(options.length === Length.ALL) return true
+      .filter(name => {
+        if (options.length === Length.ALL) return true
         else return name.length === options.length
       })
 
-    console.log(element)
+    generatedName.value = element.map(name => {
+      return name.name
+    })
   }
-
-  
-
 </script>
 <template>
   <div class="container">
     <h1>Baby Name Generator</h1>
     <p>Choose your options and click the "Find Names" buttom below</p>
 
-    <div class="options-container">
+    <div class="options-container mb-3">
       <div class="option-container">
         <h4>1) Choose a gender</h4>
 
         <div class="option-buttons">
-          <button
-            class="option option-left"
-            :class="options.gender === Gender.BOY && 'option-active'"
-            @click="options.gender = Gender.BOY"
-          >
+          <button class="option option-left" :class="options.gender === Gender.BOY && 'option-active'"
+            @click="options.gender = Gender.BOY">
             Boy
           </button>
-           <button
-            class="option"
-            :class="options.gender === Gender.UNISEX && 'option-active'"
-            @click="options.gender = Gender.UNISEX"
-          >
+          <button class="option" :class="options.gender === Gender.UNISEX && 'option-active'"
+            @click="options.gender = Gender.UNISEX">
             Unisex
           </button>
-          <button
-            class="option option-right"
-            :class="options.gender === Gender.GIRL && 'option-active'"
-            @click="options.gender = Gender.GIRL"
-          >
+          <button class="option option-right" :class="options.gender === Gender.GIRL && 'option-active'"
+            @click="options.gender = Gender.GIRL">
             Girl
           </button>
         </div>
@@ -66,18 +61,12 @@
       <div class="option-container">
         <h4>2) Choose the name's popularity</h4>
         <div class="option-buttons">
-          <button
-            class="option option-left"
-            :class="options.popularity === Popularity.TRENDY && 'option-active'"
-            @click="options.popularity = Popularity.TRENDY"
-          >
+          <button class="option option-left" :class="options.popularity === Popularity.TRENDY && 'option-active'"
+            @click="options.popularity = Popularity.TRENDY">
             Trendy
           </button>
-          <button
-            class="option option-right"
-            :class="options.popularity === Popularity.UNIQUE && 'option-active'"
-            @click="options.popularity = Popularity.UNIQUE"
-          >
+          <button class="option option-right" :class="options.popularity === Popularity.UNIQUE && 'option-active'"
+            @click="options.popularity = Popularity.UNIQUE">
             Unique
           </button>
         </div>
@@ -86,25 +75,16 @@
       <div class="option-container">
         <h4>2) Choose name's length</h4>
         <div class="option-buttons">
-          <button
-            class="option option-left"
-            :class="options.length === Length.LONG && 'option-active'"
-            @click="options.length = Length.LONG"
-          >
+          <button class="option option-left" :class="options.length === Length.LONG && 'option-active'"
+            @click="options.length = Length.LONG">
             Long
           </button>
-          <button
-            class="option"
-            :class="options.length === Length.ALL && 'option-active'"
-            @click="options.length = Length.ALL"
-          >
+          <button class="option" :class="options.length === Length.ALL && 'option-active'"
+            @click="options.length = Length.ALL">
             All
           </button>
-          <button
-            class="option option-right"
-            :class="options.length === Length.SHORT && 'option-active'"
-            @click="options.length = Length.SHORT"
-          >
+          <button class="option option-right" :class="options.length === Length.SHORT && 'option-active'"
+            @click="options.length = Length.SHORT">
             Short
           </button>
         </div>
@@ -113,71 +93,23 @@
       <button class="primary" @click="filterName">Find Names</button>
 
     </div>
-    
-    {{generatedName}}
+
+    <table class="table">
+      <thead>
+        <th>S.No</th>
+        <th>Name</th>
+      </thead>
+      <tbody>
+        <tr v-for="(name, index) in generatedName" :key="index">
+          <td data-label="S.No"> {{index}} </td>
+          <td data-label="Name"> {{name}} </td>
+        </tr>
+      </tbody>
+    </table>
 
   </div>
 </template>
 
 <style scoped>
-  .container {
-    font-family: Arial, Helvetica, sans-serif;
-    color: rgb(27, 60, 138);
-    max-width: 50rem;
-    margin: 0 auto;
-    text-align: center;
-  }
-
-  h1 {
-    font-size: 3rem;
-  }
-
-  .options-container {
-    background-color: rgb(255, 238, 236);
-    border-radius: 2rem;
-    padding: 1rem;
-    width: 95%;
-    margin: 0 auto;
-    margin-top: 4rem;
-    position: relative;
-  }
-
-  .option-container {
-    margin-bottom: 2rem;
-  }
-
-  .option {
-    background: white;
-    outline: 0.15rem solid rgb(249, 87, 89);
-    border: none;
-    padding: 0.75rem;
-    width: 12rem;
-    font-size: 1rem;
-    color: rgb(27, 60, 138);
-    cursor: pointer;
-    font-weight: 200;
-  }
-
-  .option-left {
-    border-radius: 1rem 0 0 1rem;
-  }
-
-  .option-right {
-    border-radius: 0 1rem 1rem 0;
-  }
-
-  .option-active {
-    background-color: rgb(249, 87, 89);
-    color: white;
-  }
-  .primary {
-  background-color: rgb(249, 87, 89);
-  color: white;
-  border-radius: 6.5rem;
-  border: none;
-  padding: 0.75rem 4rem;
-  font-size: 1rem;
-  margin-top: 1rem;
-  cursor: pointer;
-}
+  @import url('@/assets/style.css');
 </style>
